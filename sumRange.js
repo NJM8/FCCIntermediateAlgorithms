c1c823e41579350ef9da8034b0da3e95e7ebf894
Nate Mallison <healthynate@gmail.com>
2018-03-22 09:20:49 -0400

    completed sumrange challenge

---
+++ sumRange.js
@@ -0,0 +1,25 @@
+function sumAll(arr) {
+  return arr.reduce((sum, element, index) => {
+    if (index === 0) {
+      if (element > arr[1]) {
+        for (let i = element; i > arr[index + 1]; i--) {
+          sum += i;
+        }
+        return sum;
+      } else {
+        for (let j = element; j < arr[index + 1]; j++) {
+          sum += j;
+        }
+        return sum;
+      }
+    } else {
+      sum += element;
+      return sum;
+    }
+  }, 0);
+}
+
+console.log(sumAll([1, 4]));
+console.log(sumAll([4, 1]));
+console.log(sumAll([5, 10]));
+console.log(sumAll([10, 5]));
